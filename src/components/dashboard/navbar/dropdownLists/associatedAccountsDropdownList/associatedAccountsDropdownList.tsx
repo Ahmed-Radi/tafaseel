492ccf8683511cde5402b941f64041a81a6e453a
ahmed-radi <ahmedradi743@gmail.com>
2024-05-29 02:08:49 +0300

    Add dashboard navbar and dropdown lists for user profile and associated accounts

---
+++ src/components/dashboard/navbar/dropdownLists/associatedAccountsDropdownList/associatedAccountsDropdownList.tsx
@@ -0,0 +1,43 @@
+import {
+	Menu,
+	MenuItem,
+	MenuButton,
+	MenuDivider,
+	MenuHeader,
+} from "@szhsin/react-menu";
+import "@szhsin/react-menu/dist/index.css";
+import "@szhsin/react-menu/dist/transitions/slide.css";
+import { Box } from "@chakra-ui/react";
+import CurrentAccounts from "./currentAccounts/currentAccounts";
+import styles from "./associatedAccountsDropdownList.module.scss";
+
+type Props = {};
+
+function AssociatedAccountsDropdownList({}: Props) {
+	const { menu_container, szh_menu__item } = styles;
+	return (
+		<Menu
+			menuButton={
+				<MenuButton>
+					<CurrentAccounts />
+				</MenuButton>
+			}
+			menuClassName={menu_container}>
+			<MenuItem className={szh_menu__item} href={"user/profile"}>
+				<Box as={"span"}>API DATA</Box>
+			</MenuItem>
+			<MenuItem className={szh_menu__item} href={"user/profile"}>
+				<Box as={"span"}>API DATA</Box>
+			</MenuItem>
+			<MenuItem className={szh_menu__item} href={"user/profile"}>
+				<Box as={"span"}>API DATA</Box>
+			</MenuItem>
+			<MenuDivider />
+			<MenuItem className={szh_menu__item} href={"/user/device/create"}>
+				<Box as={"span"}>+</Box>
+			</MenuItem>
+		</Menu>
+	);
+}
+
+export default AssociatedAccountsDropdownList;
